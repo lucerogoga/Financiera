cf93a8c5f08c41e5c43ae7bb6815c16f326a1989
Usuario <Usuario@BEPIS09>
2016-08-17 21:40:32 -0500

    Agregar archivos de proyecto.

---
+++ Financiera.Dominio/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Financiera.Dominio
+{
+    public class Cliente
+    {
+        public int CodigoCliente {get; set; }
+      
+        public int NombreCliente { get; set; }
+
+        public int TipoCliente { get; set; }
+
+        public void Daralta(String as_nombre , byte aby_tipo)
+        {
+            return new Cliente()
+            {
+                NombreCliente = as_nombre ,
+                TipoCliente = aby_tipo
+            };
+        }
+
+        public void ModificarNombre(String as_nombre)
+        {
+     
+        }
+    }
+}
---
+++ Financiera.Dominio/Cuentacorriente.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Financiera.Dominio
+{
+    public class Cuentacorriente
+    {
+        public int NroCuenta
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Saldo
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int FechadeApertura
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Estado
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public Cliente Cliente
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void Aperturar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Cancelar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Bloquear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Desbloquear()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
